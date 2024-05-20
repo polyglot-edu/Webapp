f94a8cf0deffc458580a85e8de612fb3e76f80dd
tmaog <tommasoguidolin01@gmail.com>
2024-05-20 12:09:08 +0200

    alert removed

--- src/FlowListMenu.js
+++ src/FlowListMenu.js
@@ -55,7 +55,6 @@ function FlowListMenu() {
                   return;
                 }
                 WA.player.state.actualFlow = flow._id;
-                alert("Learning Path chosen correctly");
               }}
             >
               SELECT LP
