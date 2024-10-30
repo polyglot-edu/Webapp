2efbcf53e87feb9104af122adff5977ded9e3d9a
RikiStiz <riccardo.stiz6@gmail.com>
2024-10-30 14:12:57 +0100

    feat: iframe yt

--- src/components/ActivityTypes/watchVideo.tsx
+++ src/components/ActivityTypes/watchVideo.tsx
@@ -23,8 +23,9 @@ const WatchVideoTool = ({
 }: WatchVideoToolProps) => {
   if (!isOpen) return <></>;
   console.log('data check ' + actualActivity);
-  const data =
-    actualActivity?.data || ({ text: '', link: '' } as WatchVideoData);
+  const data = 
+  actualActivity?.data || ({ text: '', link: '' } as WatchVideoData);
+  const isYouTubeLink = data.link.includes('youtube.com');
   // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
     if (!data) return;
@@ -43,35 +44,58 @@ const WatchVideoTool = ({
       <HeadingTitle>Watch Video Activity</HeadingTitle>
       <HeadingSubtitle>Watch the video at the following link</HeadingSubtitle>
       <br />
-      
-      <Link 
-        href={data.link} 
-        target="_blank"
-        color="#0890d3" 
-        fontSize="lg" 
-        fontWeight="bold"
-        width="50%"
-        mt={6}
-        p={5}
-        borderRadius="md"
-        bg="gray.100"
-        boxShadow="md"
-        border="2px solid transparent"
-        display="flex"
-        flexDirection="row"
-        alignItems="center"
-        justifyContent="center"
-        gap={2}
-        _hover={{ 
-          bg: "gray.200",           
-          boxShadow: "xl",  
-          transition:"all 0.2s ease-in-out",
-          textDecoration: "underline", 
-          borderColor: "#0890d3",        
-        }}
-      >
-        Watch video <ArrowRightIcon/>
-      </Link>    
+
+      {isYouTubeLink ? (
+        <Box
+          position="relative"
+          width="100%"
+          paddingBottom="56.25%"
+          overflow="hidden"
+          borderRadius="md"
+          boxShadow="md"
+          mt={2}
+        >
+          <Box
+            as="iframe"
+            src={data.link.replace("watch?v=", "embed/")}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            position="absolute"
+            width="100%"
+            height="100%"
+          />
+        </Box>
+      ) : (
+        <Link 
+          href={data.link} 
+          target="_blank"
+          color="#0890d3" 
+          fontSize="lg" 
+          fontWeight="bold"
+          width="50%"
+          mt={6}
+          p={5}
+          borderRadius="md"
+          bg="gray.100"
+          boxShadow="md"
+          border="2px solid transparent"
+          display="flex"
+          flexDirection="row"
+          alignItems="center"
+          justifyContent="center"
+          gap={2}
+          _hover={{ 
+            bg: "gray.200",           
+            boxShadow: "xl",  
+            transition:"all 0.2s ease-in-out",
+            textDecoration: "underline", 
+            borderColor: "#0890d3",        
+          }}
+        >
+          Watch video <ArrowRightIcon/>
+        </Link> 
+      )}
     </Box>
     
   );
