e82665f0e32f5b79aa379f5fc67283d80d6623a2
tmaog <tommasoguidolin01@gmail.com>
2024-12-06 13:03:28 +0100

    feat: nodes reorder based on execution

---
+++ src/algorithms/flowAlgo.ts
@@ -0,0 +1,57 @@
+import { PolyglotEdge, PolyglotNode } from '../types/polyglotElements';
+export type FlowOrderFunction= {
+    orderedNodes: PolyglotNode[];
+    message: string;
+    error: number;
+}
+export function flowLearningExecutionOrder(
+  nodes: PolyglotNode[],
+  edges: PolyglotEdge[]
+): FlowOrderFunction{
+  //this algorithm returns an array of nodes in the correct order of execution
+
+  const orderedNodes: PolyglotNode[] = [];
+
+  //search for first nodes
+  const firstNode = nodes.find((node) => {
+    if (edges.find((edge) => edge.reactFlow.target == node._id)) return false;
+    return true;
+  });
+
+  if (!firstNode)
+    return {
+      orderedNodes: orderedNodes,
+      message: 'This flow has no starting point.',
+      error: 404,
+    };
+
+  orderedNodes.push(firstNode);
+
+  do {
+    const missingNodes = nodes.filter((node) =>
+      orderedNodes.find((index) => index._id == node._id)
+    );
+    missingNodes.forEach((node) => {
+      if (orderedNodes.find((index) => index._id == node._id)) return;
+      if (
+        edges.find((edge) => {
+          if (
+            orderedNodes.findIndex(
+              (index) =>
+                edge.reactFlow.source == index._id &&
+                edge.reactFlow.target == node._id
+            )
+          )
+            return true;
+        })
+      )
+        orderedNodes.push(node);
+    });
+  } while (nodes.length == orderedNodes.length);
+
+  return {
+    orderedNodes: orderedNodes,
+    message: 'This flow is complete.',
+    error: 200,
+  };
+}
